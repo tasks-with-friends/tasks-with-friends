3a8027a8406d75704573428d0ab10d43503f5dc9
Steve Konves <stephen.konves@gmail.com>
2021-12-22 15:53:52 -0700

    feat: persist users using fauna

--- src/registry.ts
+++ src/registry.ts
@@ -5,10 +5,13 @@ import { FsUsersService } from './services/fs-users-service';
 import { join } from 'path';
 
 import { IUsersService } from './domain';
+import { Client } from 'faunadb';
+import { FaunaUsersService } from './services/fauna-users-service';
 
 export type ServiceMap = {
   'current-user-id': string;
   'users-service': IUsersService;
+  fauna: Client;
 };
 
 export const registry = new Registry<ServiceMap>();
@@ -20,4 +23,14 @@ registry
 
 registry
   .for('users-service')
-  .use(() => new FsUsersService(join(__dirname, '_temp_users.json')));
+  // .use(() => new FsUsersService(join(__dirname, '_temp_users.json')));
+  .use((get) => new FaunaUsersService(get('fauna')));
+
+registry.for('fauna').use(
+  () =>
+    new Client({
+      secret: process.env.FAUNA_DB_SECRET || '',
+      domain: process.env.FAUNA_DB_DOMAIN || '',
+      scheme: 'https',
+    }),
+);
