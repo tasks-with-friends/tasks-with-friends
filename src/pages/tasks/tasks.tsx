318352c9a45c949c7db28d08eb2e052910441784
Steve Konves <stephen.konves@gmail.com>
2022-01-07 20:29:17 -0700

    refactor: move TaskList to its own file

--- src/pages/tasks/tasks.tsx
+++ src/pages/tasks/tasks.tsx
@@ -1,16 +1,11 @@
-import React, { useMemo } from 'react';
+import React from 'react';
 import { useQuery, gql } from '@apollo/client';
-import { ChevronRightIcon, PlusIcon } from '@heroicons/react/solid';
+import { PlusIcon } from '@heroicons/react/solid';
 import { Link } from 'react-router-dom';
 
 import { Page } from '../../templates/page';
-import {
-  GetTasksQuery,
-  GetTasksQuery_tasks_nodes,
-} from './__generated__/GetTasksQuery';
-import { useProfile } from '../../profile-provider';
-import { ParticipantResponse } from '../../../__generated__/globalTypes';
-import { useTaskModal } from '../../components/task-modal';
+import { GetTasksQuery } from './__generated__/GetTasksQuery';
+import { TaskList } from '../../components/task-list';
 
 export const GET_TASKS = gql`
   query GetTasksQuery {
@@ -55,78 +50,6 @@ const CreateTaskButton: React.VFC<{ className?: string }> = ({ className }) => (
   </Link>
 );
 
-const TaskItem: React.VFC<{ task: GetTasksQuery_tasks_nodes }> = ({ task }) => {
-  const { TaskModal, open } = useTaskModal();
-
-  const profile = useProfile();
-
-  const me = useMemo(
-    () => task.participants.nodes.find((p) => p.user.id === profile.id),
-    [profile.id, task.participants.nodes],
-  );
-
-  return (
-    <>
-      <li key={task.id}>
-        <a onClick={open} className="cursor-pointer block hover:bg-gray-50">
-          <div className="px-4 py-4 flex items-center sm:px-6">
-            <div className="min-w-0 flex-1 sm:flex sm:items-center sm:justify-between">
-              <div className="truncate">
-                <div className="flex text-sm">
-                  <p className="font-medium text-indigo-600 truncate">
-                    {task.name}
-                    {!!me?.response && (
-                      <span
-                        className={`uppercase ml-1 inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium  ${
-                          me.response === ParticipantResponse.YES
-                            ? 'bg-green-100 text-green-800'
-                            : 'bg-red-100 text-red-800'
-                        }`}
-                      >
-                        {me.response}
-                      </span>
-                    )}
-                  </p>
-                  <p className="ml-1 flex-shrink-0 font-normal text-gray-500">
-                    {task.groupSize === 1
-                      ? 'Just me'
-                      : `${task.groupSize} of us`}{' '}
-                    for {task.durationMinutes}m
-                  </p>
-                </div>
-                <div className="mt-2 flex">
-                  <div className="flex items-center text-sm text-gray-500">
-                    <p>{task.description}</p>
-                  </div>
-                </div>
-              </div>
-              <div className="mt-4 flex-shrink-0 sm:mt-0 sm:ml-5">
-                <div className="flex overflow-hidden -space-x-1">
-                  {task.participants.nodes.map((node) => (
-                    <img
-                      key={node.user.id}
-                      className="inline-block h-6 w-6 rounded-full ring-2 ring-white"
-                      src={node.user.avatarUrl || ''}
-                      alt={node.user.name}
-                    />
-                  ))}
-                </div>
-              </div>
-            </div>
-            <div className="ml-5 flex-shrink-0">
-              <ChevronRightIcon
-                className="h-5 w-5 text-gray-400"
-                aria-hidden="true"
-              />
-            </div>
-          </div>
-        </a>
-      </li>
-      <TaskModal task={task} />
-    </>
-  );
-};
-
 const TasksGuts: React.VFC = () => {
   const { loading, error, data } = useQuery<GetTasksQuery>(GET_TASKS);
 
@@ -165,13 +88,7 @@ const TasksGuts: React.VFC = () => {
   return (
     <>
       <CreateTaskButton className="mb-4" />
-      <div className="bg-white shadow overflow-hidden sm:rounded-md">
-        <ul role="list" className="divide-y divide-gray-200">
-          {data?.tasks?.nodes.map((task) => (
-            <TaskItem key={task.id} task={task} />
-          ))}
-        </ul>
-      </div>
+      <TaskList tasks={data.tasks.nodes} />
     </>
   );
 };
